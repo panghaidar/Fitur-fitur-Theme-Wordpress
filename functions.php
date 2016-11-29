ef9a059c2fad5a678de05e2b0d7dec36c6f28844
panghaidar <ksatria_xx@yahoo.com>
2016-11-29 13:48:57 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,13 @@
 
+//cut title
+function ShortenText($text) { // Function name ShortenText
+  $chars_limit = 50; // Character length
+  $chars_text = strlen($text);
+  $text = $text." ";
+  $text = substr($text,0,$chars_limit);
+  $text = substr($text,0,strrpos($text,' '));
+
+  if ($chars_text > $chars_limit)
+     { $text = $text."..."; } // Ellipsis
+     return $text;
+}
