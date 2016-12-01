15ae33baa06717d39459776cbb7d941dcce81337
panghaidar <ksatria_xx@yahoo.com>
2016-12-01 13:58:59 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 
-//Fitur untuk memotong title
+//Fitur untuk memotong title (K01)
 function ShortenText($text) { // Function name ShortenText
   $chars_limit = 50; // Character length
   $chars_text = strlen($text);
@@ -11,3 +11,25 @@ function ShortenText($text) { // Function name ShortenText
      { $text = $text."..."; } // Ellipsis
      return $text;
 }
+// FITUR UNTUK MENAMPILKAN POPULAR POST BERDASARKAN VIEWS (K02)
+function shapeSpace_popular_posts($post_id) {
+	$count_key = 'popular_posts';
+	$count = get_post_meta($post_id, $count_key, true);
+	if ($count == '') {
+		$count = 0;
+		delete_post_meta($post_id, $count_key);
+		add_post_meta($post_id, $count_key, '0');
+	} else {
+		$count++;
+		update_post_meta($post_id, $count_key, $count);
+	}
+}
+function shapeSpace_track_posts($post_id) {
+	if (!is_single()) return;
+	if (empty($post_id)) {
+		global $post;
+		$post_id = $post->ID;
+	}
+	shapeSpace_popular_posts($post_id);
+}
+add_action('wp_head', 'shapeSpace_track_posts');
